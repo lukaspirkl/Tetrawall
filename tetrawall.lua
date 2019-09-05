d47d71049037142cc4b0c0401b076e9fd8ef0242
Lukas Pirkl <pirkl.lukas@gmail.com>
2019-09-05 21:34:57 +0200

    Add particles effect

--- tetrawall.lua
+++ tetrawall.lua
@@ -106,6 +106,7 @@ function createBall(map, score)
             if map[self.x // 8][self.y // 8] then
                 score:add()
                 shake:start()
+                make_sparks_ps(self.x, self.y)
                 map[self.x // 8][self.y // 8] = false
                 if self.x - (self.velx * self.speed) <= (self.x // 8) * 8 then self.velx = -self.velx end
                 if self.y - (self.vely * self.speed) <= (self.y // 8) * 8 then self.vely = -self.vely end
@@ -326,6 +327,7 @@ function createGameScene()
                 target:update()
                 score:update()
             end
+            update_psystems()
         end,
         draw = function()
             cls(0)
@@ -342,6 +344,7 @@ function createGameScene()
             for _,b in pairs(balls)   do b:draw() end
             target:draw()
             score:draw()
+            draw_psystems()
         end
     }
 end
@@ -451,3 +454,369 @@ function TIC()
     --print(c,0,0,15)
 end
 
+
+
+
+--==================================================================================--
+-- PARTICLE SYSTEM LIBRARY =========================================================--
+--==================================================================================--
+
+particle_systems = {}
+
+-- Call this, to create an empty particle system, and then fill the emittimers, emitters,
+-- drawfuncs, and affectors tables with your parameters.
+function make_psystem(minlife, maxlife, minstartsize, maxstartsize, minendsize, maxendsize)
+	local ps = {
+	-- global particle system params
+
+	-- if true, automatically deletes the particle system if all of it's particles died
+	autoremove = true,
+
+	minlife = minlife,
+	maxlife = maxlife,
+
+	minstartsize = minstartsize,
+	maxstartsize = maxstartsize,
+	minendsize = minendsize,
+	maxendsize = maxendsize,
+
+	-- container for the particles
+	particles = {},
+
+	-- emittimers dictate when a particle should start
+	-- they called every frame, and call emit_particle when they see fit
+	-- they should return false if no longer need to be updated
+	emittimers = {},
+
+	-- emitters must initialize p.x, p.y, p.vx, p.vy
+	emitters = {},
+
+	-- every ps needs a drawfunc
+	drawfuncs = {},
+
+	-- affectors affect the movement of the particles
+	affectors = {},
+	}
+
+	table.insert(particle_systems, ps)
+
+	return ps
+end
+
+-- Call this to update all particle systems
+function update_psystems()
+	local timenow = time()
+	for key,ps in pairs(particle_systems) do
+		update_ps(ps, timenow)
+	end
+end
+
+-- updates individual particle systems
+-- most of the time, you don't have to deal with this, the above function is sufficient
+-- but you can call this if you want (for example fast forwarding a particle system before first draw)
+function update_ps(ps, timenow)
+	for key,et in pairs(ps.emittimers) do
+		local keep = et.timerfunc(ps, et.params)
+		if (keep==false) then
+			table.remove(ps.emittimers, key)
+		end
+	end
+
+	for key,p in pairs(ps.particles) do
+		p.phase = (timenow-p.starttime)/(p.deathtime-p.starttime)
+
+		for key,a in pairs(ps.affectors) do
+			a.affectfunc(p, a.params)
+		end
+
+		p.x = p.x + p.vx
+		p.y = p.y + p.vy
+
+		local dead = false
+		if (p.x<0 or p.x>240 or p.y<0 or p.y>136) then
+			dead = true
+		end
+
+		if (timenow>=p.deathtime) then
+			dead = true
+		end
+
+		if (dead==true) then
+			table.remove(ps.particles, key)
+		end
+	end
+
+	if (ps.autoremove==true and #ps.particles<=0) then
+		local psidx = -1
+		for pskey,pps in pairs(particle_systems) do
+			if pps==ps then
+				table.remove(particle_systems, pskey)
+				return
+			end
+		end
+	end
+end
+
+-- draw a single particle system
+function draw_ps(ps, params)
+	for key,df in pairs(ps.drawfuncs) do
+		df.drawfunc(ps, df.params)
+	end
+end
+
+-- draws all particle system
+-- This is just a convinience function, you probably want to draw the individual particles,
+-- if you want to control the draw order in relation to the other game objects for example
+function draw_psystems()
+	for key,ps in pairs(particle_systems) do
+		draw_ps(ps)
+	end
+end
+
+-- This need to be called from emitttimers, when they decide it is time to emit a particle
+function emit_particle(psystem)
+	local p = {}
+
+	local ecount = nil
+	local e = psystem.emitters[math.random(#psystem.emitters)]
+	e.emitfunc(p, e.params)
+
+	p.phase = 0
+	p.starttime = time()
+	p.deathtime = time()+frnd(psystem.maxlife-psystem.minlife)+psystem.minlife
+
+	p.startsize = frnd(psystem.maxstartsize-psystem.minstartsize)+psystem.minstartsize
+	p.endsize = frnd(psystem.maxendsize-psystem.minendsize)+psystem.minendsize
+
+	table.insert(psystem.particles, p)
+end
+
+function frnd(max)
+	return math.random()*max
+end
+
+
+--================================================================--
+-- MODULES =======================================================--
+--================================================================--
+
+-- You only need to copy the modules you actually use to your program
+
+
+-- EMIT TIMERS ==================================================--
+
+-- Spawns a bunch of particles at the same time, then removes itself
+-- params:
+-- num - the number of particle to spawn
+function emittimer_burst(ps, params)
+	for i=1,params.num do
+		emit_particle(ps)
+	end
+	return false
+end
+
+-- Emits a particle every "speed" time
+-- params:
+-- speed - time between particle emits
+function emittimer_constant(ps, params)
+	if (params.nextemittime<=time()) then
+		emit_particle(ps)
+		params.nextemittime = params.nextemittime + params.speed
+	end
+	return true
+end
+
+-- EMITTERS =====================================================--
+
+-- Emits particles from a single point
+-- params:
+-- x,y - the coordinates of the point
+-- minstartvx, minstartvy and maxstartvx, maxstartvy - the start velocity is randomly chosen between these values
+function emitter_point(p, params)
+	p.x = params.x
+	p.y = params.y
+
+	p.vx = frnd(params.maxstartvx-params.minstartvx)+params.minstartvx
+	p.vy = frnd(params.maxstartvy-params.minstartvy)+params.minstartvy
+end
+
+-- Emits particles from the surface of a rectangle
+-- params:
+-- minx,miny and maxx, maxy - the corners of the rectangle
+-- minstartvx, minstartvy and maxstartvx, maxstartvy - the start velocity is randomly chosen between these values
+function emitter_box(p, params)
+	p.x = frnd(params.maxx-params.minx)+params.minx
+	p.y = frnd(params.maxy-params.miny)+params.miny
+
+	p.vx = frnd(params.maxstartvx-params.minstartvx)+params.minstartvx
+	p.vy = frnd(params.maxstartvy-params.minstartvy)+params.minstartvy
+end
+
+-- AFFECTORS ====================================================--
+
+-- Constant force applied to the particle troughout it's life
+-- Think gravity, or wind
+-- params:
+-- fx and fy - the force vector
+function affect_force(p, params)
+	p.vx = p.vx + params.fx
+	p.vy = p.vy + params.fy
+end
+
+-- A rectangular region, if a particle happens to be in it, apply a constant force to it
+-- params:
+-- zoneminx, zoneminy and zonemaxx, zonemaxy - the corners of the rectangular area
+-- fx and fy - the force vector
+function affect_forcezone(p, params)
+	if (p.x>=params.zoneminx and p.x<=params.zonemaxx and p.y>=params.zoneminy and p.y<=params.zonemaxy) then
+		p.vx = p.vx + params.fx
+		p.vy = p.vy + params.fy
+	end
+end
+
+-- A rectangular region, if a particle happens to be in it, the particle stops
+-- params:
+-- zoneminx, zoneminy and zonemaxx, zonemaxy - the corners of the rectangular area
+function affect_stopzone(p, params)
+	if (p.x>=params.zoneminx and p.x<=params.zonemaxx and p.y>=params.zoneminy and p.y<=params.zonemaxy) then
+		p.vx = 0
+		p.vy = 0
+	end
+end
+
+-- A rectangular region, if a particle cames in contact with it, it bounces back
+-- params:
+-- zoneminx, zoneminy and zonemaxx, zonemaxy - the corners of the rectangular area
+-- damping - the velocity loss on contact
+function affect_bouncezone(p, params)
+	if (p.x>=params.zoneminx and p.x<=params.zonemaxx and p.y>=params.zoneminy and p.y<=params.zonemaxy) then
+		p.vx = -p.vx*params.damping
+		p.vy = -p.vy*params.damping
+	end
+end
+
+-- A point in space which pulls (or pushes) particles in a specified radius around it
+-- params:
+-- x,y - the coordinates of the affector
+-- radius - the size of the affector
+-- strength - push/pull force - proportional with the particle distance to the affector coordinates
+function affect_attract(p, params)
+	if (math.abs(p.x-params.x)+math.abs(p.y-params.y)<params.mradius) then
+		p.vx = p.vx + (p.x-params.x)*params.strength
+		p.vy = p.vy + (p.y-params.y)*params.strength
+	end
+end
+
+-- Moves particles around in a sin/cos wave or circulary. Directly modifies the particle position
+-- params:
+-- speed - the effect speed
+-- xstrength, ystrength - the amplituse around the x and y axes
+function affect_orbit(p, params)
+	params.phase = params.phase + params.speed
+	p.x = p.x + math.sin(params.phase)*params.xstrength
+	p.y = p.y + math.cos(params.phase)*params.ystrength
+end
+
+-- DRAW FUNCS ===================================================--
+
+-- Filled circle particle drawer, the particle animates it's size and color trough it's life
+-- params:
+-- colors array - indexes to the palette, the particle goes trough these in order trough it's lifetime
+-- startsize and endsize is coming from the particle system parameters, not the draw func params!
+function draw_ps_fillcirc(ps, params)
+	for key,p in pairs(ps.particles) do
+		c = math.floor(p.phase*#params.colors)+1
+		r = (1-p.phase)*p.startsize+p.phase*p.endsize
+		circ(p.x,p.y,r,params.colors[c])
+	end
+end
+
+-- Single pixel particle, which animates trough the given colors
+-- params:
+-- colors array - indexes to the palette, the particle goes trough these in order trough it's lifetime
+function draw_ps_pixel(ps, params)
+	for key,p in pairs(ps.particles) do
+		c = math.floor(p.phase*#params.colors)+1
+		pix(p.x,p.y,params.colors[c])
+	end
+end
+
+-- Draws a line between the particle's previous and current position, kind of "motion blur" effect
+-- params:
+-- colors array - indexes to the palette, the particle goes trough these in order trough it's lifetime
+function draw_ps_streak(ps, params)
+	for key,p in pairs(ps.particles) do
+		c = math.floor(p.phase*#params.colors)+1
+		line(p.x,p.y,p.x-p.vx,p.y-p.vy,params.colors[c])
+	end
+end
+
+-- Animates trough the given frames with the given speed
+-- params:
+-- frames array - indexes to sprite tiles
+function draw_ps_animspr(ps, params)
+	params.currframe = params.currframe + params.speed
+	if (params.currframe>#params.frames) then
+		params.currframe = 1
+	end
+	for key,p in pairs(ps.particles) do
+		-- pal(7,params.colors[math.floor(p.endsize)])
+		spr(params.frames[math.floor(params.currframe+p.startsize)%#params.frames],p.x,p.y,0)
+	end
+	-- pal()
+end
+
+-- Maps the given frames to the life of the particle
+-- params:
+-- frames array - indexes to sprite tiles
+function draw_ps_agespr(ps, params)
+	for key,p in pairs(ps.particles) do
+		local f = math.floor(p.phase*#params.frames)+1
+		spr(params.frames[f],p.x,p.y,0)
+	end
+end
+
+-- Each particle is randomly chosen from the given frames
+-- params:
+-- frames array - indexes to sprite tiles
+function draw_ps_rndspr(ps, params)
+	for key,p in pairs(ps.particles) do
+		-- pal(7,params.colors[math.floor(p.endsize)])
+		spr(params.frames[math.floor(p.startsize)],p.x,p.y,0)
+	end
+	-- pal()
+end
+
+
+--==================================================================================--
+-- SAMPLES PARTICLE SYSTEMS ========================================================--
+--==================================================================================--
+
+function make_sparks_ps(ex,ey)
+	local ps = make_psystem(300,700, 1,2, 0.5,0.5)
+
+	table.insert(ps.emittimers,
+		{
+			timerfunc = emittimer_burst,
+			params = { num = 10 }
+		}
+	)
+	table.insert(ps.emitters,
+		{
+			emitfunc = emitter_point,
+			params = { x = ex, y = ey, minstartvx = -1.5, maxstartvx = 1.5, minstartvy = -3, maxstartvy=-2 }
+		}
+	)
+	table.insert(ps.drawfuncs,
+		{
+			drawfunc = draw_ps_fillcirc,
+			params = { colors = {15,14,12,9,4,3} }
+		}
+	)
+	table.insert(ps.affectors,
+		{
+			affectfunc = affect_force,
+			params = { fx = 0, fy = 0.3 }
+		}
+	)
+end
